2c3f14daee9a546b8961c3f937ba2e16dae93f73
jeeva2n <163100836+jeeva2n@users.noreply.github.com>
2024-03-19 20:13:29 +0530

    intern project
    landing page

---
+++ z.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL
+);
